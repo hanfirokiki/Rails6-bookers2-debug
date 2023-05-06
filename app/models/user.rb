f55e71bbe443da2aedf9b4aadfa7eb5df1cabc41
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-05-06 05:35:49 +0000

    課題5提出

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,25 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
 
-
-
-
-
   has_one_attached :profile_image
 
   validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
+
+def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+end
+
 
   def get_profile_image(width, height)
   unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
+  get "search" => "searches#search"
   get "/home/about" => "homes#about" ,as: "about"
     resources :books, only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites, only: [:create, :destroy]
@@ -11,7 +12,6 @@ Rails.application.routes.draw do
     resources :relationships, only: [:create, :destroy]
   get :followers, on: :member
   get :followeds, on: :member
-  get "search" => "searches#search"
-  end
+    end
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
